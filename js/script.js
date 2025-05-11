7fa1f2b2b2c9e1512a120f456336fe2a7816f213
ljrMy <99577343+ljrMy@users.noreply.github.com>
2025-05-11 11:03:57 +0800

    version0.1.3

--- js/script.js
+++ js/script.js
@@ -1,19 +1,41 @@
 import {weather_id, timetable, new_timetable, goal_date, goal_date_name, timetable_config} from "./const.js"
 const {ipcRenderer}=require("electron");
 
+let log=(info)=>{ipcRenderer.send("log",info)}
+
 document.getElementById("day").innerHTML=Date().split(" ")[0].toUpperCase();
 
-let xhr1=new XMLHttpRequest();
-xhr1.open(
-    'GET',
-    'https://cn.apihz.cn/api/tianqi/tqyb.php?id=88888888&key=88888888&sheng=广东&place=开平',1);
-xhr1.send();
-xhr1.onload=()=>{
-    let weather_data=JSON.parse(xhr1.response)
-    let {weather1,weather2,temperature}=weather_data;
-    document.getElementById("weather").innerHTML=`<img src="http://image.nmc.cn/assets/img/w/40x40/3/${weather_id[weather1]}.png" alt="${weather1}">`;
-    document.getElementById("temperature").innerHTML=`${temperature}℃`
+function get_weather(){
+    let xhr1=new XMLHttpRequest();
+    xhr1.open(
+        'GET',
+        'https://cn.apihz.cn/api/tianqi/tqyb.php?id=88888888&key=88888888&sheng=广东&place=开平',1);
+    xhr1.send();
+    xhr1.onload=()=>{
+        let weather_data=JSON.parse(xhr1.response)
+        let {weather1,weather2,temperature}=weather_data;
+        let img=document.querySelector("#weather img");
+        img.src=`http://image.nmc.cn/assets/img/w/40x40/3/${weather_id[weather1]}.png`;
+        img.dataset.weather1=`${weather_id[weather1]}`;
+        img.dataset.weather2=`${weather_id[weather2]}`;
+        img.dataset.show="1";
+        document.getElementById("temperature").innerHTML=`${weather_data.temperature}℃`;
+    }
 }
+get_weather()
+document.getElementById("calendar").addEventListener("click",()=>{
+    let img=document.querySelector("#weather img");
+    if(img.dataset.show=='1'){
+        img.src=`http://image.nmc.cn/assets/img/w/40x40/3/${img.dataset.weather2}.png`;
+        img.dataset.show='2';
+    }else{
+        img.src=`http://image.nmc.cn/assets/img/w/40x40/3/${img.dataset.weather1}.png`;
+        img.dataset.show='1';
+    }
+})
+document.getElementById("calendar").addEventListener("dblclick",()=>{
+    get_weather()
+})
 
 let date=new Date(),day=date.getDay(),full_date=date.getFullYear()*1e4+(date.getMonth()+1)*1e2+date.getDate();
 let today_timetable;
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const {app,BrowserWindow,screen,ipcMain,Menu,Tray} = require('electron');
 const {exec}=require("child_process")
+const path=require("path")
 var win=undefined;
 
 const createWindow = () => {
@@ -15,18 +16,42 @@ const createWindow = () => {
     alwaysOnTop: true,
     maximizable: false,
     minimizable: false,
+    skipTaskbar: true,
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false
+      contextIsolation: false,
+      webSecurity: false,
     }
   })
   win.loadFile('./index.html');
-  // win.setIgnoreMouseEvents(true);
+  win.setAlwaysOnTop(true, 'screen-saver');
+}
+
+const createTray = () =>{
+  let tray=new Tray(path.join(__dirname,'static','icon64.ico'))
+  const contextmenu=Menu.buildFromTemplate([
+    {
+      label: '退出',
+      click: ()=>{
+        app.quit()
+      }
+    },
+    {
+      label: '显示/隐藏',
+      click: ()=>{
+        win.isVisible()?win.hide():win.show()
+      }
+    }
+  ]);
+  tray.setContextMenu(contextmenu);
 }
 
 app.whenReady().then(() => {
+  app.setAppUserModelId('com.ljr.classyc');
   createWindow();
+  createTray();
 })
+
 
 ipcMain.on('set-ignore', (event, ignore) => {
   if(ignore) win.setIgnoreMouseEvents(true,{forward:true});
